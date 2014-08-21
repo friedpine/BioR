2004a43f655f26c7a5179e1bb556299b332c0cf0
friedpine <friedpine@gmail.com>
2014-08-21 15:11:14 +0800

    Samples_files_R

--- BioR/SUPER_SEQ_SAMPLES.R
+++ BioR/SUPER_SEQ_SAMPLES.R
@@ -1,1 +1,33 @@
-GEO_samples <- read.delim("E:/bdyun/00.lab/31.cRNA/samples/GEO_samples.txt")
+library(RODBC)
+GEO_samples <- read.delim("E:/bdyun/00.lab/31.cRNA/samples/GEO_samples_2.txt",header=F)
+GEO_samples$names = gsub(".R1.fastq.gz","",GEO_samples$V4)
+GEO_fq1 = subset(GEO_samples,V2=="fq1" | V2=="raw_fq1")
+
+
+library(RODBC)
+channel <- odbcConnect("p00", uid="root", pwd="123456")
+
+samples = sqlQuery(channel,"select * from p01.samples")
+merged = merge(samples,GEO_fq1,by.x="exp",by.y="V1")
+colnames(merged) = c(colnames(merged)[1:12],"GEO_name") 
+sqlUpdate(channel,merged[,c(1:8,13)],tablename="p01.samples")
+
+samples = sqlQuery(channel,"select * from p01.samples where length(geo_name)>2")
+
+#HEK_CELLS
+FPKM_super_HEK = sqlQuery(channel,"select * from p01.HEK_FPKM_gencode")
+for (i in colnames(FPKM_super_HEK)[4:21]){
+  geo_name = subset(samples,exp==i)[1,9]
+  tmp = FPKM_super_HEK[,c("gene","cat","genename",i)]
+  colnames(tmp) = c("ENSEMBL_ID","category","gene_name","FPKM")
+  write.table(x=tmp,file=paste(geo_name,"_FPKM_gencode.txt",sep=""))
+}
+
+FPKM_super_mouse = sqlQuery(channel,"select * from p01.p01_FPKM")
+for (i in colnames(FPKM_super_mouse)[5:56]){
+  geo_name = subset(samples,exp==i)[1,9]
+  tmp = FPKM_super_mouse[,c("genename",i)]
+  colnames(tmp) = c("gene_name","FPKM")
+  write.table(x=tmp,file=paste(geo_name,"_FPKM_gencode.txt",sep=""))
+}
+
--- BioR/Samples_files_management.R
+++ BioR/Samples_files_management.R
@@ -2,10 +2,12 @@
 #INPUT2: files lists RAW_sample names and official names maybe other infos(rawname & sample)
 
 fastq_file_lists = function(samples,files,types){
-  files$rawname = ldply(strsplit(as.character(files$path),"/"),function(x) x[length(x)-1])
+  files = files[,c(5,9)]
+  colnames(files) = c("size","path")
+  files$rawname = ldply(strsplit(as.character(files$path),"/"),function(x) x[length(x)-1])[,1]
   files$type = types
   merged = merge(files,samples,by="rawname")
   df_up = data.frame(sample=merged$sample,type=merged$type,
-                    path=merged$path,method="A",total_reads=1000000)
+                    path=merged$path,method="A",total_reads=10000000)
   return(df_up)
 }
